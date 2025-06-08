939628b94e4a5dcbe7b9523b507e318e8f88fb66
kinannac <kinantiayuc2405@gmail.com>
2025-06-08 08:49:49 +0700

    Update A06_ProgramKomnum_11.py

--- A06_ProgramKomnum_11.py
+++ A06_ProgramKomnum_11.py
@@ -2,6 +2,12 @@ import numpy as np
 
 x = 5
 
+xi = (np.sqrt( x*x*x/-6 + 19*x/6 + 14))
+
 for i in range(5):
+    ea = np.abs(xi - x)*100/xi
     x = (np.sqrt( x*x*x/-6 + 19*x/6 + 14))
-    print (np.round(x,2), "->", x)
+    xi = (np.sqrt( xi*xi*xi/-6 + 19*xi/6 + 14))
+    print ("Iterasi", i+1, "=", np.round(x,2))
+    print ("Ea =", np.round(ea,2),"%")
+    print ("")
